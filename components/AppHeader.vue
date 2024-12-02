0b4e70261055449bbd8e8842b9c8ec2960e9e352
David <fernando.gam@outlook.com>
2024-12-02 17:13:27 -0500

    added navigation to appheader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
     <ul class="nav justify-content-end bg-primary-subtle py-2 px-3">
         <li class="nav-item me-3">
-            <a class="nav-link active" aria-current="page" href="/">Principal</a>
+            <NuxtLink class="nav-link active" aria-current="page" to="/">Principal</NuxtLink>
         </li>
         <li class="nav-item me-3">
-            <a class="nav-link"  href="/create-task">Crer tarea</a>
+            <NuxtLink class="nav-link"  to="/create-task">Crer tarea</NuxtLink>
         </li>
         <li class="nav-item me-3">
-            <a class="nav-link"   href="/about">Acerca de </a>
+            <NuxtLink class="nav-link"  to="/about" >Acerca de </NuxtLink>
         </li>
     </ul>
 </template>
